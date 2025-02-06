2840d6c0a548456b0f2775ef23ef90ab00d22cc7
Mikky <miamvi09@gmail.com>
2025-02-06 09:47:38 -0500

    Revert to original

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,11 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 # pylint: disable=fixme, line-too-long
-# ALLOWED_HOSTS = ['localhost',
-# 'https://mihikadagar-8000.theiadockernext-
-# 0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-# CSRF_TRUSTED_ORIGINS = ['https://mihikadagar-8000.theiadockernext-
-# 0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = ['localhost', 'https://mihikadagar-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://mihikadagar-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,6 +93,10 @@ DATABASES = {
 }
 
 AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
     {
         'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
